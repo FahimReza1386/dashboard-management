7eba8ddff4fa9ced409ca7291a4b9d17d9a40259
FahimReza <FahimReza20200@gmail.com>
2025-12-17 01:17:50 +0330

    fix: fixed bug for celery and install requirements

--- core/core/settings.py
+++ core/core/settings.py
@@ -177,8 +177,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Celery Configurations
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/2'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
